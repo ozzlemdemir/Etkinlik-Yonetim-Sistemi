10a7766db01ddac1b0a98665a13fbc4caa612834
ozzlemdemir <146757725+ozzlemdemir@users.noreply.github.com>
2025-10-11 19:36:18 +0300

    Giriş sayfası oluşturuldu ve indexe yönlendirildi.

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect, url_for, request, make_response
 
 # Create an instance of the Flask class
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return ("hello.html")
+def hello():
+    return render_template("hello.html")
 
 @app.route('/login')
 def login():
-    return ("login.html")
+    return render_template("login.html")
+
+@app.route('/index', methods=['POST'])
+def index():
+     ContexData={
+    'email' :request.form["email"],
+    'password' :request.form["password"],
+    }
+     return render_template("index.html",**ContexData)
 
 # Run the application
 if __name__ == '__main__':
